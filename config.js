528a0d9a2f9f77a761e9f2950ac50d083007fe75
Shrunga <shrungashree889@gmail.com>
2022-11-24 10:42:59 +0530

    Removed the keys

--- config.js
+++ config.js
@@ -4,13 +4,7 @@ import "firebase/compat/firestore";
 
 
 const firebaseConfig ={
-  apiKey: "AIzaSyC_nsBa2l-nM1kw650MZ7zU7gK3cLinWVE",
-  authDomain: "todo-b2c56.firebaseapp.com",
-  projectId: "todo-b2c56",
-  storageBucket: "todo-b2c56.appspot.com",
-  messagingSenderId: "63406682844",
-  appId: "1:63406682844:web:5e3c7fbf337daabfdd3a19",
-  measurementId: "G-89ZVFJXQF9"
+  //Add your firebase configuration keys here
 };
 
 if (!firebase.apps.length){
